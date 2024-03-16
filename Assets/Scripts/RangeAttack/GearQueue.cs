8005e8c49d773ec02c5ef5e7e47c1d5797d0938b
empieniazek <empieniazek@student.agh.edu.pl>
2024-03-16 21:18:24 +0100

    Tmp added

--- Assets/Scripts/RangeAttack/GearQueue.cs
+++ Assets/Scripts/RangeAttack/GearQueue.cs
@@ -1,18 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GearQueue : MonoBehaviour
 {
+    private UiGearQueueDisplay uiGearQueueDisplay;
     private Queue<GameObject> _gearQueue = new Queue<GameObject>();
-    
+
+    private void Start() {
+        uiGearQueueDisplay = FindObjectOfType<UiGearQueueDisplay>();
+    }
+
     public void AddGear(GameObject gear) {
         _gearQueue.Enqueue(gear);
+        UpdateUiGears();
     }
     
     public bool TryGetNextGear(out GameObject gear) {
         if (HasAnyGear()) {
             gear = _gearQueue.Dequeue();
+            UpdateUiGears();
             return true;
         }
         gear = null;
@@ -21,6 +29,7 @@ public class GearQueue : MonoBehaviour
     
     public void ClearGearQueue() {
         _gearQueue.Clear();
+        UpdateUiGears();
     }
 
     public void SetGearSetup(List<GameObject> gearSet) {
@@ -28,6 +37,7 @@ public class GearQueue : MonoBehaviour
         foreach (var gear in gearSet) {
             _gearQueue.Enqueue(gear);
         }
+        UpdateUiGears();
     }
 
     public bool HasSpaceForNextGear(int maxSize) {
@@ -36,5 +46,19 @@ public class GearQueue : MonoBehaviour
 
     public bool HasAnyGear() {
         return _gearQueue.Count > 0;
+    }
+    
+    private void UpdateUiGears() {
+        var gears = _gearQueue.ToArray();
+        List<GameObject> gearToDisplay = new List<GameObject>();
+        for (int i = 0; i < 3; i++) {
+            if(i < gears.Length) {
+                gearToDisplay.Add(gears[i]);
+            }
+            else {
+                break;
+            }
+        }
+        uiGearQueueDisplay.SetGearImages(gearToDisplay);
     }
 }
