d56ae46389be0ac1d732a2bca0e7490a6f6cc53a
Mikosztyla <mikosztyla@gmail.com>
2024-03-16 17:35:34 +0100

    Range attack anim

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -9,34 +9,72 @@ public class Player : MonoBehaviour
     [SerializeField] private int meleeDmg;
     [SerializeField] private int weaponDmg;
     [SerializeField] private float meleeAttackCooldown;
-    [SerializeField] private float movementSpeed;
+    [SerializeField] private float movementSpeed = 2f;
+    [SerializeField] private int numberOfJumps = 1;
+    [SerializeField] private float jumpForce = 2f;
+
 
     [SerializeField] private BoxCollider2D meleeAttackCollider;
 
     private bool isAttacking;
-    private Animator animator;
     private Vector2 movement;
     private Rigidbody2D rb;
+    private PlayerAnimations animations;
+    private bool canJump = false;
+    private bool isGrounded = true;
+    private int jumpsLeft;
 
     private void Start()
     {
         isAttacking = false;
         InputManager.input.Player.MeleeAttack.performed += MeleeAttack;
-        animator = GetComponent<Animator>();
+        InputManager.input.Player.RangeAttack.performed += RangeAttack;
         rb = GetComponent<Rigidbody2D>();
+        animations = GetComponent<PlayerAnimations>();
+        jumpsLeft = numberOfJumps;
     }
 
     private void FixedUpdate()
     {
         movement = InputManager.navigationAxis;
+        if (movement.y == 0)
+        {
+            canJump = true;
+        }
         rb.velocity = new Vector2(-movement.x * movementSpeed * Time.deltaTime * 100, rb.velocity.y);
+
+        if (Mathf.Abs(rb.velocity.y) < 0.01f)
+        {
+            isGrounded = true;
+            jumpsLeft = numberOfJumps;
+            canJump = true;
+        }
+
+        if (movement.y > 0 && (isGrounded || jumpsLeft > 0) && canJump)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            isGrounded = false; 
+            jumpsLeft -= 1;
+            canJump = false;
+            animations.Jump();
+        }
     }
 
     void MeleeAttack(UnityEngine.InputSystem.InputAction.CallbackContext context)
     {
         if (isAttacking) return;
         isAttacking = true;
+        animations.AttackMelee();
         meleeAttackCollider.enabled = true;
+        StartCoroutine(Attack(meleeAttackCooldown));
+        StartCoroutine(MeleeHitBoxDisable(0.05f));
+    }
+
+    void RangeAttack(UnityEngine.InputSystem.InputAction.CallbackContext context)
+    {
+        if (isAttacking) return;
+        isAttacking = true;
+        animations.AttackRange();
         StartCoroutine(Attack(meleeAttackCooldown));
     }
 
@@ -46,17 +84,22 @@ public class Player : MonoBehaviour
         {
             collision.gameObject.GetComponent<Enemy>().TakeDamage(meleeDmg + weaponDmg);
         }
-        Debug.Log("hit");
     }
 
     IEnumerator Attack(float seconds)
     {
         yield return new WaitForSeconds(seconds);
         isAttacking = false;
+    }
+
+    IEnumerator MeleeHitBoxDisable(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
         meleeAttackCollider.enabled = false;
-        animator.SetBool("isAttacking", false);
     }
 
     public Vector2 GetMovement() { return movement; }
+    public Vector2 GetVelocity() { return rb.velocity; }
     public bool GetIsAttacking() { return isAttacking; }
+    public bool GetIsGrounded() {  return isGrounded; }
 }
---
+++ Assets/Scripts/Player/PlayerAnimations.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimations : MonoBehaviour
+{
+    private Animator animator;
+    private Vector2 velocity;
+    private Vector2 movement;
+    private Player player;
+
+    private bool isFacingRight = false;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        player = GetComponent<Player>();
+    }
+
+    void Update()
+    {
+        FlipSprite();
+    }
+
+    private void FixedUpdate()
+    {
+        velocity = player.GetVelocity();
+        animator.SetFloat("xVelocity", Mathf.Abs(velocity.x));
+        animator.SetFloat("yVelocity", velocity.y);
+        animator.SetBool("isJumping", !player.GetIsGrounded());
+    }
+
+    void FlipSprite()
+    {
+        movement = player.GetMovement();
+        if (isFacingRight && movement.x < 0 || !isFacingRight && movement.x > 0)
+        {
+            isFacingRight = !isFacingRight;
+            transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+        }    
+    }
+
+    public void AttackMelee()
+    {
+        animator.SetTrigger("AttackMelee");
+    }
+
+    public void AttackRange() {
+        animator.SetTrigger("AttackRange");
+    }
+
+    public void Jump()
+    {
+        animator.SetTrigger("Jump");
+    }
+}
