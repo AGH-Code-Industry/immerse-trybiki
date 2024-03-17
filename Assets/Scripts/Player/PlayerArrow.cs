79e0b8b3f0c6e6cd4c0bebcc5df9d27f91b274dd
KacperSobolak <kacpersobolak03@gmail.com>
2024-03-17 13:58:30 +0100

    arrow

---
+++ Assets/Scripts/Player/PlayerArrow.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerArrow : MonoBehaviour {
+    private Enemy _following;
+
+    [SerializeField]
+    private float distanceToHide;
+    private bool _startFollow;
+
+    private SpriteRenderer arrowImage;
+    
+    public void StartPointing(Enemy enemyToFollow) {
+        arrowImage = GetComponentInChildren<SpriteRenderer>();
+        _following = enemyToFollow;
+        _startFollow = true;
+        enemyToFollow.OnEnemyDeath += EnemyToFollowOnOnEnemyDeath;
+    }
+
+    private void EnemyToFollowOnOnEnemyDeath(object sender, EventArgs e) {
+        DestroyArrow();
+    }
+
+    private void Update() {
+        if (_startFollow) {
+            if (Vector2.Distance(transform.position, _following.transform.position) < distanceToHide) {
+                Hide();
+            }
+            else {
+                float angle = Vector2.SignedAngle(Vector2.up, _following.transform.position - transform.position);
+                transform.eulerAngles = new Vector3(0f, 0f, angle);
+                Show();
+            }
+        }
+    }
+
+    private void DestroyArrow() {
+        Destroy(gameObject);
+    }
+
+    private void Hide() {
+        arrowImage.gameObject.SetActive(false);
+    }
+
+    private void Show() {
+        arrowImage.gameObject.SetActive(true);
+    }
+}
