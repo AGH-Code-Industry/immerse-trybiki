4f80fdf2ee8dcfb7730cec7f3c51f6637651ad6f
empieniazek <empieniazek@student.agh.edu.pl>
2024-03-17 07:47:31 +0100

    Rework sepcial effect

--- Assets/Scripts/RangeAttack/GearSpecialAction/DoNothing.cs
+++ Assets/Scripts/RangeAttack/GearSpecialAction/DoNothing.cs
@@ -1,6 +1,16 @@
-﻿namespace RangeAttack.GearSpecialAction {
+﻿using System;
+using UnityEngine;
+
+namespace RangeAttack.GearSpecialAction {
     public class DoNothing : GearSpecialAction {
+        private ParticleSystem _particleSystem;
+
+        private void Start() {
+            _particleSystem = transform.GetComponentInChildren<ParticleSystem>();
+        }
+
         public override void Invoke() {
+            _particleSystem.Play();
             return;
         }
     }
