1d46696600a9a0ab15052c5110a4239ef96ba8a8
empieniazek <empieniazek@student.agh.edu.pl>
2024-03-17 14:53:39 +0100

    Menu utils

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    
+    public void StartGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Level1");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
